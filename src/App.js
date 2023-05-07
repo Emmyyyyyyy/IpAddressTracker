f316cb07ba345bf235d20a20c96d7b26437d8a1e
Emmy <eemmyy.a@gmail.com>
2023-05-07 16:41:13 +0700

    Finish static web

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import icon from "../src/images/icon-arrow.svg"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='wrapper'>
+        <h1 className='test'>IP Address Tracker</h1>
+        <div className='input-wrapper'>
+          <input placeholder='Search for any IP address or domain' className='input-box'></input>
+          <div className='button'>
+            <img src={icon} alt='icon'></img>
+          </div>
+        </div>
+        <div className='info-wrapper'>
+          <div className='info-box'>
+            <div className='box'>
+              <p className='info-head'>IP ADDRESS</p>
+              <p className='info-text'>192.212.174.101</p>
+            </div>
+            <div className='devided-line'></div>
+            <div className='box'>
+              <p className='info-head'>LOCATION</p>
+              <p className='info-text'>Brooklyn, NY 10001</p>
+            </div>
+            <div className='devided-line'></div>
+            <div className='box'>
+              <p className='info-head'>TIMEZONE</p>
+              <p className='info-text'>UTC-05:00</p>
+            </div>
+            <div className='devided-line'></div>
+            <div className='box'>
+              <p className='info-head'>ISP</p>
+              <p className='info-text'>SpaceX Starlink</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
